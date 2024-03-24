fa8f03ece03ad541abfb61fe9c30825655250351
Om Patel <omupatel7113@gmail.com>
2024-03-24 23:39:52 +0530

    Setup

--- app.js
+++ app.js
@@ -1,27 +1,25 @@
-const express = require('express');
-require('dotenv').config()
-const { default: mongoose } = require('mongoose');
-var cors = require('cors')
+const express = require("express");
+require("dotenv").config();
+const { default: mongoose } = require("mongoose");
+var cors = require("cors");
 const app = express();
 const port = 8000; // Define your desired port
 
-
 // Middleware to parse JSON requests
-app.use(express.json(),cors());
+app.use(express.json(), cors());
 const uri = process.env.MONGODB_URL; // Replace with your MongoDB URI and database name
 
 //connecting MongoDB
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
- const db=mongoose.connection;
+mongoose.connect(uri);
+const db = mongoose.connection;
 
-db.once('open',()=>console.log("Connected"))
-
+db.once("open", () => console.log("Database Connected Successfully!"));
 
 // Example route
-app.get('/', (req, res) => {
-  res.send('Hello, this is your Express app!');
+app.get("/", (req, res) => {
+  res.send("Hello, this is your Express app!");
 });
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
